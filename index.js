5ed715a60209f74ed317bdb60df8a5a3d4cf5752
sonsu95 <tjdtn951007@gmail.com>
2023-03-26 20:19:50 +0900

    imsi wq

---
+++ index.js
@@ -0,0 +1,17 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('./text.txt').toString().split(' ')
+// console.log(input);
+const fs = require('fs');
+const input = fs.readFileSync('./text.txt').toString().split('\n');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+const result1 = a * parseInt(input[1][2]);
+const result2 = a * parseInt(input[1][1]);
+const result3 = a * parseInt(input[1][0]);
+const result4 = a * b;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
