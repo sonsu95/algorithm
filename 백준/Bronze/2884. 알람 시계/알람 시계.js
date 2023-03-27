c415a377ff0e32a428aa2a317356e9f1bfcb2f90
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-03-27 10:34:34 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -4,25 +4,11 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const hour = Number(input[0].split(' ')[0]);
 const minute = Number(input[0].split(' ')[1]);
 const setAlarm45MinuteEarlier = (hour, minute) => {
-    let resultHour;
-    let resultMinute;
-    if (hour === 0) {
-        if (minute >= 45) {
-            resultHour = hour;
-            resultMinute = minute - 45;
-        } else {
-            resultHour = 24 - (Math.abs(hour - 1));
-            resultMinute = minute + 15;
-        }
-    } else {
-        if (minute >= 45) {
-            resultHour = hour;
-            resultMinute = minute - 45;
-        } else {
-            resultHour = hour - 1;
-            resultMinute = minute + 15;
-        }
-    }
-    console.log(resultHour, resultMinute);
+    if (minute < 45) {
+        hour -= 1;
+        minute += 15;
+        if (hour < 0) hour = 23;
+    } else minute -= 45;
+    console.log(hour, minute);
 }
 setAlarm45MinuteEarlier(hour, minute);
