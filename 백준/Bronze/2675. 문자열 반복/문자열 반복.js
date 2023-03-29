f068edda0b07524daec473b8883640f21454740a
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-03-30 01:49:03 +0900

    [Bronze II] Title: 문자열 반복, Time: 124 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -2,16 +2,13 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./index.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-for (let i = 0; i < input.length - 1; i++) {
-    const resultArr = [];
-    const n = Number(input[i][0]);
-    const data = input[i].split(' ');
-    if (data[1]) {
-        for (let j = 0; j < data[1].length; j++) {
-            for (let k = 0; k < n; k++) {
-                resultArr.push(data[1][j]);
-            }
-        }
-        console.log(resultArr.join(''));
-    }
+const n = Number(input[0]);
+for (let i = 1; i <= n; i++) {
+
+let [r, s] = input[i].split(' ');
+let result = '';
+for (let j = 0; j < s.length; j++) {
+    result += s.charAt(j).repeat(r);
+}
+console.log(result)
 }
