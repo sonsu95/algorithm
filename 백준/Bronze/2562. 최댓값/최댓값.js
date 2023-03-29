b2f7367289cb74623ae09d2c364dd6eed2257736
sonsu95 <tjdtn951007@gmail.com>
2023-03-29 22:29:29 +0900

    답안 추가

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,9 +1,24 @@
 const fs = require("fs");
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
+
+/**
+ * Case 1
+ * Memory: 9328kb
+ * Time: 120ms
+ */
 const arr = input.map(Number);
 let max = arr[0];
 arr.forEach((num, idx) => {
     max <= num ? max = num : null;
 })
+console.log(max + '\n' + (arr.indexOf(max) + 1));
+
+/**
+ * Case 2
+ * Memory: 9324kb
+ * Time: 120ms
+ */
+const arr = input.map(Number);
+let max = Math.max(...arr);
 console.log(max + '\n' + (arr.indexOf(max) + 1));
