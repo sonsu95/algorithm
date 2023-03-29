3e7163499a092b818a8d690ddea97a73c4d01792
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-03-29 10:54:38 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,6 @@
+function solution(array) {
+    let map = new Map();
+    for (let num of array) map.set(num, (map.get(num) || 0) + 1);
+    map = [...map].sort((a, b) => b[1] - a[1]);
+    return map.length === 1  || map[0][1] > map[1][1] ? map[0][0] : -1;
+}
