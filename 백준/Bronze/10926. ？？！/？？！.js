426c85e583a5705b5925be8294d34fce5676626f
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-04-02 14:34:48 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./index.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const data = input[0].trim() + "??!";
+
+console.log(data)
