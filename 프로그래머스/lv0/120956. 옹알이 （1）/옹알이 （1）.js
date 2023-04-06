d9267b118bd6c0174b69f0d5cfff8a0eba96105b
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-04-07 08:27:16 +0900

    [level 0] Title: 옹알이 (1), Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,8 @@
+function solution(babbling) {
+    let answer = 0;
+    const regex = /^(aya|ye|woo|ma)+$/;
+    babbling.forEach(word => {
+        if (regex.test(word)) answer++;
+    })
+    return answer;
+}
