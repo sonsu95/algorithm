f2fa79a064cddb9a5da31b0bd1aed06708437a8a
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-04-07 11:17:19 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,10 +1,8 @@
 function solution(rsp) {
-    const newArr = [];
-
-    rsp.split("").forEach(item => {
-        if (item === "2") newArr.push("0");
-        else if (item === "0") newArr.push("5");
-        else newArr.push("2");
-    })
-    return newArr.join("");
+    const obj = {
+        0: 5,
+        2: 0,
+        5: 2
+    }
+    return [...rsp].map(v => obj[v]).join("")
 }
