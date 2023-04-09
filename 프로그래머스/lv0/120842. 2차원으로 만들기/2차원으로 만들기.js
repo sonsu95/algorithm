a294bff692caf6e6729b51c62c0dd2e67ae3efa7
sonsu95 <tjdtn951007@gmail.com>
2023-04-09 10:51:20 +0900

    2차원으로 만들기 추가 답안

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -9,3 +9,9 @@ function solution(num_list, n) {
     }
     return newArr;
 }
+
+
+function solution2(num_list, n) {
+    // splice는 원본 배열을 건드려버리니 map함수로 순회를 할때마다 원본 배열을 토막쳐버리기에 index를 0으로 두고 시작한다.
+    return Array(num_list.length / n).fill([]).map(x => num_list.splice(0, n))
+}
