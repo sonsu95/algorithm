60c3e6a1feff5ecee57f17c15ab40350a9dcb749
sonsu95 <127682098+sonsu95@users.noreply.github.com>
2023-04-14 04:17:03 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,24 +1,3 @@
 function solution(bin1, bin2) {
-    function binaryToDecimal(binary) {
-        let decimal = 0;
-        let power = 0;
-        for (let i = binary.length - 1; i >= 0; i--) {
-            if (binary[i] === '1') {
-                decimal += Math.pow(2, power);
-            }
-            power++;
-        }
-        return decimal;
-    }
-
-    function decimalToBinary(decimal) {
-        let binary = '';
-        while (decimal > 0) {
-            binary = (decimal % 2) + binary;
-            decimal = Math.floor(decimal / 2);
-        }
-        return binary || '0'; // return '0' for decimal value of 0
-    }
-
-    return decimalToBinary(binaryToDecimal(bin1) + binaryToDecimal(bin2));
+    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
 }
