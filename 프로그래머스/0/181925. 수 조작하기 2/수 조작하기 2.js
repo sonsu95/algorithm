6e071f8d370716d777d8ad8cfde61a8f540b894f
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-04-20 19:56:59 +0900

    [level 0] Title: 수 조작하기 2, Time: 14.09 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,20 +1,6 @@
 function solution(numLog) {
-    let result = '';
-    
-    const operations = {
-        'w': 1,
-        'a': -10,
-        's': -1,
-        'd': 10
-    };
-    
-    for (let i = 0; i < numLog.length; i++) {
-        const cal = numLog[i] - numLog[i-1];
-        if (operations.w === cal) result += 'w';
-        if (operations.a === cal) result += 'a';
-        if (operations.s === cal) result += 's';
-        if (operations.d === cal) result += 'd';
-    }
-    
-    return result;
+    return numLog.slice(1).map((v, i) => {
+        const diff = v - numLog[i];
+        return { 1: 'w', '-10': 'a', '-1': 's', 10: 'd' }[diff];
+    }).join('');
 }
