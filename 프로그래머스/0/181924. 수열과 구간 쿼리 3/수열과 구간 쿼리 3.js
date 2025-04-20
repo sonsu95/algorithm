3214add51f7efb491422fdefbeaa787d2a0a961b
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-04-20 20:14:36 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.59 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,9 @@
 function solution(arr, queries) {
-    const result = [...arr]; 
+    const result = [...arr];
     
-    for (let i = 0; i < queries.length; i++) {
-        const [index1, index2] = queries[i];
-        [result[index1], result[index2]] = [result[index2], result[index1]];
-    }
+    queries.forEach(([i, j]) => {
+        [result[i], result[j]] = [result[j], result[i]];
+    });
     
     return result;
 }
