944dc4978a5a2dbd6f2c9d6b6ee16999bbe06f7b
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-04-20 13:16:33 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,3 +1,6 @@
 function solution(num_list) {
-    return num_list.reduce((acc, cur, idx, arr) => +arr.filter(num => num % 2 === 0).join('') + +arr.filter(num => num % 2 === 1).join(''), 0)
+    const evens = num_list.filter(num => num % 2 === 0).join('');
+    const odds = num_list.filter(num => num % 2 === 1).join('');
+    
+    return Number(evens) + Number(odds);
 }
