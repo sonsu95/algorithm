48c015a491a4eb82990d7d0cb9e761df960cc460
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-04-28 01:18:43 +0900

    Time: 1 ms (60.46%), Space: 59.2 MB (46.13%) - LeetHub

--- LeetCode/0412-fizz-buzz/0412-fizz-buzz.ts
+++ LeetCode/0412-fizz-buzz/0412-fizz-buzz.ts
@@ -1,9 +1,11 @@
 function fizzBuzz(n: number): string[] {
     const result: string[] = []
     for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) result.push('FizzBuzz');
-        else if (i % 3 === 0) result.push('Fizz');
-        else if (i % 5 === 0) result.push('Buzz');
+        const isDiv3 = i % 3 === 0;
+        const isDiv5 = i % 5 === 0;
+        if (isDiv3 && isDiv5) result.push('FizzBuzz');
+        else if (isDiv3) result.push('Fizz');
+        else if (isDiv5) result.push('Buzz');
         else result.push(String(i));
     }
     return result;
