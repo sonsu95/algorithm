f6400dc40ddedcd8bbeaae8ff1f2cb427f764c5b
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-04-28 01:02:34 +0900

    Time: 0 ms (100%), Space: 55 MB (82.13%) - LeetHub

--- LeetCode/1791-richest-customer-wealth/1791-richest-customer-wealth.ts
+++ LeetCode/1791-richest-customer-wealth/1791-richest-customer-wealth.ts
@@ -1,13 +1,3 @@
 function maximumWealth(accounts: number[][]): number {
-    const resultArr: number[] = [];
-
-    for (let i = 0; i < accounts.length; i++) {
-        let sum = 0;
-        for (let j = 0; j < accounts[i].length; j++) {
-            sum += accounts[i][j];
-        }
-        resultArr.push(sum);
-    }
-
-    return Math.max(...resultArr);
+     return Math.max(...accounts.map(account => account.reduce((acc, cur)=> acc + cur,0)));
 };
