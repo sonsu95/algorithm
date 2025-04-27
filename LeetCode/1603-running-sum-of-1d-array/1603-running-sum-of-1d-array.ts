21868d7956e541f782d3b25dc0d11545a18ca2be
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-04-28 00:45:18 +0900

    Time: 0 ms (100%), Space: 56 MB (92.4%) - LeetHub

--- LeetCode/1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.ts
+++ LeetCode/1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.ts
@@ -1,10 +1,4 @@
 function runningSum(nums: number[]): number[] {
-    const result = [];
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (i === 0) result.push(nums[0]);
-        else result.push(nums[i] + result[i-1]);
-    }
-    
-    return result;
+     let result: number = 0;
+     return nums.map(item => result += item);
 };
