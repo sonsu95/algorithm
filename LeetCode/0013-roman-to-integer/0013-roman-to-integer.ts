9e8ca5d918745dc9610d990e0e5e9df6a056d22b
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-05-01 01:34:03 +0900

    Time: 15 ms (10.35%), Space: 62.7 MB (34.66%) - LeetHub

--- LeetCode/0013-roman-to-integer/0013-roman-to-integer.ts
+++ LeetCode/0013-roman-to-integer/0013-roman-to-integer.ts
@@ -1,26 +1,15 @@
+const roman = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
 function romanToInt(s: string): number {
-    const map = new Map([
-        ['I', 1],
-        ['V', 5],
-        ['X', 10],
-        ['L', 50],
-        ['C', 100],
-        ['D', 500],
-        ['M', 1000]
-    ]);
-
-    let result = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        const currentVal = map.get(s[i]);
-        const nextVal = i + 1 < s.length ? map.get(s[i + 1]) : 0;
-
-        if (currentVal < nextVal) {
-            result -= currentVal;
-        } else {
-            result += currentVal;
-        }
-    }
-    
-    return result;
+  const integers = s.split('').map(c => roman[c]);
+  
+  return integers.reduce((acc, x, i) => x < integers[i + 1] ? acc - x : acc + x, 0);
 };
