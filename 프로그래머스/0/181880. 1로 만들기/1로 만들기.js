32bebca703c208f3985f94923efa06e585afaef1
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-05-08 01:13:05 +0900

    [level 0] Title: 1로 만들기, Time: 0.15 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,13 +1,13 @@
 function solution(num_list) {
-    let cnt = 0;
+    let count = 0;
     
-    for(let i = 0; i < num_list.length; i++) {
-        while (num_list[i] > 1) {
-            if (num_list[i] % 2 ===  0) num_list[i] /= 2;
-            else num_list[i] = (num_list[i] - 1) / 2;
-            cnt += 1;
+    for (let num of num_list) {
+        while (num > 1) {
+            if (num % 2 === 0)  num /= 2;
+            else num = (num - 1) / 2;            
+            count++;
         }
     }
     
-    return cnt;
+    return count;
 }
