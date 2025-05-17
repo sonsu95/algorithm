685ae404069e65d033cf1bde3318e350df9da15e
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-05-18 00:38:46 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,8 @@
+function solution(angle) {
+    if (0 < angle && angle < 90) return 1;
+    else if (angle === 90) return 2;
+    else if (90 < angle && angle < 180) return 3;
+    else if (angle === 180) return 4;
+    
+    throw new Error('예상치 못한 angle값입니다.');
+}
