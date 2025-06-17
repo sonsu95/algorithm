d8326bb2a7ba2ca7dc036d2cec776ae821e356aa
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-06-18 08:18:18 +0900

    Time: 0 ms (100%), Space: 54.1 MB (89.98%) - LeetHub

---
+++ LeetCode/1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,15 @@
+function numberOfSteps(num: number): number {
+    let cnt = 0;
+    let calNum = num;
+
+    while(calNum !== 0) {
+        if (calNum % 2 === 0) {
+            calNum /= 2;
+        } else {
+            calNum -= 1;
+        }
+        cnt++;
+    }
+    
+    return cnt;
+};
