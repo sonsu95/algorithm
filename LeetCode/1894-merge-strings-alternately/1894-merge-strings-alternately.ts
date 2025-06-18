d52fbf7f19ab400c33809d3592e5bb0c64022553
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-06-18 09:23:37 +0900

    Time: 48 ms (39.45%), Space: 56.3 MB (24.31%) - LeetHub

--- LeetCode/1894-merge-strings-alternately/1894-merge-strings-alternately.ts
+++ LeetCode/1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -1,10 +1,10 @@
 function mergeAlternately(word1: string, word2: string): string {
-    const result = [];
+    let result = '';
 
     for (let i = 0; i < word1.length + word2.length; i++) {
-        if (word1[i]) result.push(word1[i]);
-        if (word2[i]) result.push(word2[i]);
+        if (word1[i]) result += word1[i];
+        if (word2[i]) result += word2[i];
     }
 
-    return result.join('');
+    return result;
 };
