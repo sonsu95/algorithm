a0a42dfa35ee27bc38a0026bd619e533b8635478
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-06-18 16:46:22 +0900

    Time: 0 ms (100%), Space: 55.3 MB (81.11%) - LeetHub

---
+++ LeetCode/1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.ts
@@ -0,0 +1,11 @@
+function gcd(a: number, b: number): number {
+    return b === 0 ? a : gcd(b, a % b);
+}
+
+function gcdOfStrings(str1: string, str2: string): string {
+    if (str1 + str2 !== str2 + str1) return '';
+
+    const gcdLength = gcd(str1.length, str2.length);
+
+    return str1.substring(0, gcdLength);
+};
