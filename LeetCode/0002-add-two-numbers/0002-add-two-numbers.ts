8de1e7146591b4d1320a9a73909abbc2326534ec
Sungsu <127682098+sonsu95@users.noreply.github.com>
2025-06-22 23:49:53 +0900

    Time: 4 ms (52.77%), Space: 61.8 MB (82.38%) - LeetHub

---
+++ LeetCode/0002-add-two-numbers/0002-add-two-numbers.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let dummyNode = new ListNode(0);
+    let current = dummyNode;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
+
+        const sum = val1 + val2 + carry; 
+        carry = Math.floor(sum / 10);
+        const digit = sum % 10;
+
+        current.next = new ListNode(digit);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    return dummyNode.next;
+};
